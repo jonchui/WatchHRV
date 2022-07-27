dedfa9eaf3259b5208cccfa8cc13969385cff0e7
Jon Chui <jon.chui@gmail.com>
2022-07-27 11:29:10 -0600

    Confirmed iOS 15 sleep qualit is only 0 = in bed, 1= asleep... and not seeing any 2 = awake yet... which sucks TBH
    https://www.loom.com/share/d63364e501ea4e298ed019a4296af662
    
    slack: https://wonderandwander.slack.com/archives/C03QPKV0DK5/p1658942767421719

--- WatchHRV/TableViewController.swift
+++ WatchHRV/TableViewController.swift
@@ -41,6 +41,11 @@ class TableViewController: UITableViewController {
             return
         }
         
+        guard let _ = HKObjectType.categoryType(forIdentifier: .sleepAnalysis) else {
+            print("sleep analysis not allowed")
+            return
+        }
+        
         let dataTypes: Set<HKQuantityType> = [hrQuantityType, hrvQuantityType]
         
         healthStore.requestAuthorization(toShare: nil, read: dataTypes) { (success, error) -> Void in
@@ -53,7 +58,73 @@ class TableViewController: UITableViewController {
                 print("not allowed")
             }
         }
+        
+        requestSleepAuthorization();
     }
+    
+    func requestSleepAuthorization() {
+        
+        if let sleepType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis) {
+            let setType = Set<HKSampleType>(arrayLiteral: sleepType)
+            healthStore.requestAuthorization(toShare: setType, read: setType) { (success, error) in
+                
+                if !success || error != nil {
+                    // handle error
+                    return
+                }
+                
+                // handle success
+                let day = Date(timeIntervalSinceNow: -7*24*60*60)
+                self.readSleep(from: day, to: nil)
+            }
+        }
+    }
+    
+    func readSleep(from startDate: Date?, to endDate: Date?) {
+        
+        let healthStore = HKHealthStore()
+        
+        // first, we define the object type we want
+        guard let sleepType = HKObjectType.categoryType(forIdentifier: .sleepAnalysis) else {
+            return
+        }
+        
+        // we create a predicate to filter our data
+        let predicate = HKQuery.predicateForSamples(withStart: startDate, end: endDate, options: .strictStartDate)
+
+        // I had a sortDescriptor to get the recent data first
+        let sortDescriptor = NSSortDescriptor(key: HKSampleSortIdentifierEndDate, ascending: false)
+
+        // we create our query with a block completion to execute
+        let query = HKSampleQuery(sampleType: sleepType, predicate: predicate, limit: 30, sortDescriptors: [sortDescriptor]) { (query, result, error) in
+            if error != nil {
+                // handle error
+                return
+            }
+            
+            if let result = result {
+                
+                // do something with those data
+                result
+                    .compactMap({ $0 as? HKCategorySample })
+                    .forEach({ sample in
+                        guard let sleepValue = HKCategoryValueSleepAnalysis(rawValue: sample.value) else {
+                            return
+                        }
+                        
+                        let isAsleep = sleepValue == .asleep
+                        
+                        print("sleep status: \(sleepValue) Start: \(sample.startDate) \(sample.endDate) - source \(sample.sourceRevision.source.name) - isAsleep \(isAsleep)")
+                        
+                    })
+            }
+        }
+
+        // finally, we execute our query
+        healthStore.execute(query)
+    }
+
+
     
     @objc private func refreshHRVData(_ sender: Any) {
         hrvData.removeAll()
